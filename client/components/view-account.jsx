af2fd759ebd4c596109307bf946381fe255b7735
Jiheng Nie <Niejiheng@gmail.com>
2022-11-17 19:54:32 +0000

    Compleated user can log out feature

--- client/components/view-account.jsx
+++ client/components/view-account.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Smiley from './smiley';
 import AccountCard from './account-card';
+import Sidebar from './sidebar';
 
 export default class ViewAccount extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentRating: null,
+      sidebar: false,
       userLoggedIn: JSON.parse(window.localStorage.getItem('account'))
     };
 
     this.displayingPage = this.displayingPage.bind(this);
+    this.handleUserClick = this.handleUserClick.bind(this);
+  }
+
+  handleUserClick() {
+    this.setState({ sidebar: !this.state.sidebar });
   }
 
   componentDidUpdate(prevProps) {
@@ -52,18 +59,25 @@ export default class ViewAccount extends React.Component {
   }
 
   displayingPage() {
-    return (<div className='container'>
-      <div className='row flex-center'>
-        <div className='column-third-always left-align '>
-          <a href='#view-other-accounts'><i className="fa-solid fa-house-chimney fa-3x fa-house-style" /></a>
+    return (
+      <>
+        <div className='container'>
+          <div className='row flex-center'>
+            <div className='column-third-always left-align '>
+              <a href='#view-other-accounts'><i className="fa-solid fa-house-chimney fa-3x fa-house-style" /></a>
+            </div>
+            <div className='column-third-always'>
+              <Smiley currentRating={this.state.currentRating} />
+            </div>
+            <div className='column-third-always'>
+              <i onClick={this.handleUserClick} className="fa-solid fa-users  fa-3x fa-users-style" />
+            </div>
+          </div>
+          <AccountCard username={this.props.username} view="current-user" hideNewRating={true} displayCurrentUserRating={true} />
         </div>
-        <div className='column-third-always'>
-          <Smiley currentRating={this.state.currentRating} />
-        </div>
-        <div className='column-third-always' />
-      </div>
-      <AccountCard username={this.props.username} view="current-user" hideNewRating={true} displayCurrentUserRating={true} />
-    </div>);
+        {this.state.sidebar ? <Sidebar closeSidebar={this.state.sidebar} handleChange={this.handleUserClick}/> : null}
+      </>
+    );
   }
 
   render() {
--- client/components/view-other-account.jsx
+++ client/components/view-other-account.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Smiley from './smiley';
 import AccountCard from './account-card';
+import Sidebar from './sidebar';
 
 export default class ViewOtherAccount extends React.Component {
   constructor(props) {
@@ -10,9 +11,15 @@ export default class ViewOtherAccount extends React.Component {
       otherUsers: null,
       currentRating: null,
       userLoggedIn: JSON.parse(window.localStorage.getItem('account')),
+      sidebar: false,
       currentIndex: 1
     };
     this.handleDirectionClick = this.handleDirectionClick.bind(this);
+    this.handleUserClick = this.handleUserClick.bind(this);
+  }
+
+  handleUserClick() {
+    this.setState({ sidebar: !this.state.sidebar });
   }
 
   handleDirectionClick(event) {
@@ -86,28 +93,33 @@ export default class ViewOtherAccount extends React.Component {
         userNextIndex = otherUsers[0];
       }
       return (
-        <div className='container'>
-          <div className='row flex-center'>
-            <div className='column-third-always left-align '>
-              <a href='#view-other-accounts'><i className="fa-solid fa-house-chimney fa-3x fa-house-style" /></a>
+        <>
+          <div className='container'>
+            <div className='row flex-center'>
+              <div className='column-third-always left-align '>
+                <a href='#view-other-accounts'><i className="fa-solid fa-house-chimney fa-3x fa-house-style" /></a>
+              </div>
+              <div className='column-third-always'>
+                <Smiley currentRating={this.state.currentRating}/>
+              </div>
+              <div className='column-third-always'>
+                <i onClick={this.handleUserClick} className="fa-solid fa-users  fa-3x fa-users-style" />
+              </div>
             </div>
-            <div className='column-third-always'>
-              <Smiley currentRating={this.state.currentRating}/>
+            <div className='flex-center row-no-wrap'>
+              <i onClick={this.handleDirectionClick} className="fa-solid fa-chevron-left chevron-style left" />
+              <div className='none-focus-cards left'>
+                <AccountCard username={userPreviousIndex} hideNewRating={true} hideRating={true} hideName={true} hideStars={true} className='none-focus-cards' />
+              </div>
+              <AccountCard username={userCurrentIndex} />
+              <div className='none-focus-cards right'>
+                <AccountCard username={userNextIndex} hideNewRating={true} hideRating={true} hideName={true} hideStars={true} className='none-focus-cards overflow' />
+              </div>
+              <i onClick={this.handleDirectionClick} className="fa-solid fa-chevron-right chevron-style right" />
             </div>
-            <div className='column-third-always'/>
           </div>
-          <div className='flex-center row-no-wrap'>
-            <i onClick={this.handleDirectionClick} className="fa-solid fa-chevron-left chevron-style left" />
-            <div className='none-focus-cards left'>
-              <AccountCard username={userPreviousIndex} hideNewRating={true} hideRating={true} hideName={true} hideStars={true} className='none-focus-cards' />
-            </div>
-            <AccountCard username={userCurrentIndex} />
-            <div className='none-focus-cards right'>
-              <AccountCard username={userNextIndex} hideNewRating={true} hideRating={true} hideName={true} hideStars={true} className='none-focus-cards overflow' />
-            </div>
-            <i onClick={this.handleDirectionClick} className="fa-solid fa-chevron-right chevron-style right" />
-          </div>
-        </div>
+          {this.state.sidebar ? <Sidebar closeSidebar={this.state.sidebar} handleChange={this.handleUserClick} /> : null}
+        </>
       );
     }
   }
